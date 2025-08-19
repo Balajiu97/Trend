1aed0b43e577524962327feca15ca854ce73fefe
Balajiu97 <yuvabala355@gmail.com>
2025-08-19 22:15:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
-d
+#!/bin/bash
+
+set -e
+
+echo "Logging into EKS cluster..."
+aws eks update-kubeconfig --region ap-south-1 --name my-cluster
+
+echo "Installing kubectl..."
+chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "Applying Kubernetes manifests..."
+kubectl apply -f deployment.yaml -f service.yaml
